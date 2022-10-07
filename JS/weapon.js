6fa40b007497a039837c96c46de877ff091e91a5
duvancho303 <duvancho303@gmail.com>
2022-10-07 16:29:36 -0600

    adding code

---
+++ JS/weapon.js
@@ -0,0 +1,6 @@
+ function Weapon(classType, affinity, damage, defense) {
+    this.classType = classType;
+    this.affinity =  affinity;
+    this.damage = damage;
+    this.defense = defense; 
+ }
